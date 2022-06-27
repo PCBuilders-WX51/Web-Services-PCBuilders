1a58846ac02959f4dae1638ea23bfe5480ef60bc
FaridHinostroza <89112952+FaridHinostroza@users.noreply.github.com>
2022-06-27 00:23:24 -0500

    Delete src/companies directory

--- src/companies/application/commands/delete-company.command.ts
+++
@@ -1,3 +0,0 @@
-export class DeleteCompanyCommand {
-  constructor(readonly id: number) {}
-}
--- src/companies/domain/events/company-registered.event.ts
+++
@@ -1,9 +0,0 @@
-export class CompanyRegisteredEvent {
-  constructor(
-    public id: number,
-    public email: string,
-    public password: string,
-    public nameCompany: string,
-    public descriptionCompany: string,
-  ) {}
-}
